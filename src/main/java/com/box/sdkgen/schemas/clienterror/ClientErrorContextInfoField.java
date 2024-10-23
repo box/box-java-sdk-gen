a9e23902fd38b34c37c4dbdc97731034b4c04bda
box-sdk-build <sdk-build@box.com>
2024-10-23 05:21:36 -0700

    fix: fix additionalProperties (box/box-openapi#477)

--- src/main/java/com/box/sdkgen/schemas/clienterror/ClientErrorContextInfoField.java
+++
@@ -1,9 +0,0 @@
-package com.box.sdkgen.schemas.clienterror;
-
-public class ClientErrorContextInfoField {
-
-  @Override
-  public String toString() {
-    return "ClientErrorContextInfoField{" + "}";
-  }
-}
