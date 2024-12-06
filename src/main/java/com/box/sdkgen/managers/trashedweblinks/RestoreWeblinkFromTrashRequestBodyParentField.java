11980a48cabe158527ca05c391e2fded50a49cb6
box-sdk-build <sdk-build@box.com>
2024-12-06 07:56:00 -0800

    feat: Optional user id parameter for PUT files/folders/web_links (box/box-openapi#486)

--- src/main/java/com/box/sdkgen/managers/trashedweblinks/RestoreWeblinkFromTrashRequestBodyParentField.java
+++ src/main/java/com/box/sdkgen/managers/trashedweblinks/RestoreWeblinkFromTrashRequestBodyParentField.java
@@ -1,11 +1,15 @@
 package com.box.sdkgen.managers.trashedweblinks;
 
 import com.box.sdkgen.internal.SerializableObject;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Objects;
 
 public class RestoreWeblinkFromTrashRequestBodyParentField extends SerializableObject {
 
   protected String id;
+
+  @JsonProperty("user_id")
+  protected String userId;
 
   public RestoreWeblinkFromTrashRequestBodyParentField() {
     super();
@@ -15,10 +19,15 @@ public class RestoreWeblinkFromTrashRequestBodyParentField extends SerializableO
       RestoreWeblinkFromTrashRequestBodyParentFieldBuilder builder) {
     super();
     this.id = builder.id;
+    this.userId = builder.userId;
   }
 
   public String getId() {
     return id;
+  }
+
+  public String getUserId() {
+    return userId;
   }
 
   @Override
@@ -31,25 +40,40 @@ public class RestoreWeblinkFromTrashRequestBodyParentField extends SerializableO
     }
     RestoreWeblinkFromTrashRequestBodyParentField casted =
         (RestoreWeblinkFromTrashRequestBodyParentField) o;
-    return Objects.equals(id, casted.id);
+    return Objects.equals(id, casted.id) && Objects.equals(userId, casted.userId);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id);
+    return Objects.hash(id, userId);
   }
 
   @Override
   public String toString() {
-    return "RestoreWeblinkFromTrashRequestBodyParentField{" + "id='" + id + '\'' + "}";
+    return "RestoreWeblinkFromTrashRequestBodyParentField{"
+        + "id='"
+        + id
+        + '\''
+        + ", "
+        + "userId='"
+        + userId
+        + '\''
+        + "}";
   }
 
   public static class RestoreWeblinkFromTrashRequestBodyParentFieldBuilder {
 
     protected String id;
 
+    protected String userId;
+
     public RestoreWeblinkFromTrashRequestBodyParentFieldBuilder id(String id) {
       this.id = id;
+      return this;
+    }
+
+    public RestoreWeblinkFromTrashRequestBodyParentFieldBuilder userId(String userId) {
+      this.userId = userId;
       return this;
     }
 
--- src/main/java/com/box/sdkgen/managers/uploads/PreflightFileUploadCheckRequestBodyParentField.java
+++ src/main/java/com/box/sdkgen/managers/uploads/PreflightFileUploadCheckRequestBodyParentField.java
@@ -1,11 +1,15 @@
 package com.box.sdkgen.managers.uploads;
 
 import com.box.sdkgen.internal.SerializableObject;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Objects;
 
 public class PreflightFileUploadCheckRequestBodyParentField extends SerializableObject {
 
   protected String id;
+
+  @JsonProperty("user_id")
+  protected String userId;
 
   public PreflightFileUploadCheckRequestBodyParentField() {
     super();
@@ -15,10 +19,15 @@ public class PreflightFileUploadCheckRequestBodyParentField extends Serializable
       PreflightFileUploadCheckRequestBodyParentFieldBuilder builder) {
     super();
     this.id = builder.id;
+    this.userId = builder.userId;
   }
 
   public String getId() {
     return id;
+  }
+
+  public String getUserId() {
+    return userId;
   }
 
   @Override
@@ -31,25 +40,40 @@ public class PreflightFileUploadCheckRequestBodyParentField extends Serializable
     }
     PreflightFileUploadCheckRequestBodyParentField casted =
         (PreflightFileUploadCheckRequestBodyParentField) o;
-    return Objects.equals(id, casted.id);
+    return Objects.equals(id, casted.id) && Objects.equals(userId, casted.userId);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id);
+    return Objects.hash(id, userId);
   }
 
   @Override
   public String toString() {
-    return "PreflightFileUploadCheckRequestBodyParentField{" + "id='" + id + '\'' + "}";
+    return "PreflightFileUploadCheckRequestBodyParentField{"
+        + "id='"
+        + id
+        + '\''
+        + ", "
+        + "userId='"
+        + userId
+        + '\''
+        + "}";
   }
 
   public static class PreflightFileUploadCheckRequestBodyParentFieldBuilder {
 
     protected String id;
 
+    protected String userId;
+
     public PreflightFileUploadCheckRequestBodyParentFieldBuilder id(String id) {
       this.id = id;
+      return this;
+    }
+
+    public PreflightFileUploadCheckRequestBodyParentFieldBuilder userId(String userId) {
+      this.userId = userId;
       return this;
     }
 
