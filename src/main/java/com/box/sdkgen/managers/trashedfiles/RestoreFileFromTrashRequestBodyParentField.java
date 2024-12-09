7172e3009f8105b5c01428a84895c8dd69d33627
box-sdk-build <sdk-build@box.com>
2024-12-09 04:49:00 -0800

    feat: Optional user id parameter for PUT files/folders/web_links (box/box-openapi#486)

--- src/main/java/com/box/sdkgen/managers/trashedfiles/RestoreFileFromTrashRequestBodyParentField.java
+++ src/main/java/com/box/sdkgen/managers/trashedfiles/RestoreFileFromTrashRequestBodyParentField.java
@@ -1,11 +1,15 @@
 package com.box.sdkgen.managers.trashedfiles;
 
 import com.box.sdkgen.internal.SerializableObject;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Objects;
 
 public class RestoreFileFromTrashRequestBodyParentField extends SerializableObject {
 
   protected String id;
+
+  @JsonProperty("user_id")
+  protected String userId;
 
   public RestoreFileFromTrashRequestBodyParentField() {
     super();
@@ -15,10 +19,15 @@ public class RestoreFileFromTrashRequestBodyParentField extends SerializableObje
       RestoreFileFromTrashRequestBodyParentFieldBuilder builder) {
     super();
     this.id = builder.id;
+    this.userId = builder.userId;
   }
 
   public String getId() {
     return id;
+  }
+
+  public String getUserId() {
+    return userId;
   }
 
   @Override
@@ -31,25 +40,40 @@ public class RestoreFileFromTrashRequestBodyParentField extends SerializableObje
     }
     RestoreFileFromTrashRequestBodyParentField casted =
         (RestoreFileFromTrashRequestBodyParentField) o;
-    return Objects.equals(id, casted.id);
+    return Objects.equals(id, casted.id) && Objects.equals(userId, casted.userId);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id);
+    return Objects.hash(id, userId);
   }
 
   @Override
   public String toString() {
-    return "RestoreFileFromTrashRequestBodyParentField{" + "id='" + id + '\'' + "}";
+    return "RestoreFileFromTrashRequestBodyParentField{"
+        + "id='"
+        + id
+        + '\''
+        + ", "
+        + "userId='"
+        + userId
+        + '\''
+        + "}";
   }
 
   public static class RestoreFileFromTrashRequestBodyParentFieldBuilder {
 
     protected String id;
 
+    protected String userId;
+
     public RestoreFileFromTrashRequestBodyParentFieldBuilder id(String id) {
       this.id = id;
+      return this;
+    }
+
+    public RestoreFileFromTrashRequestBodyParentFieldBuilder userId(String userId) {
+      this.userId = userId;
       return this;
     }
 
--- src/main/java/com/box/sdkgen/managers/trashedfolders/RestoreFolderFromTrashRequestBodyParentField.java
+++ src/main/java/com/box/sdkgen/managers/trashedfolders/RestoreFolderFromTrashRequestBodyParentField.java
@@ -1,11 +1,15 @@
 package com.box.sdkgen.managers.trashedfolders;
 
 import com.box.sdkgen.internal.SerializableObject;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Objects;
 
 public class RestoreFolderFromTrashRequestBodyParentField extends SerializableObject {
 
   protected String id;
+
+  @JsonProperty("user_id")
+  protected String userId;
 
   public RestoreFolderFromTrashRequestBodyParentField() {
     super();
@@ -15,10 +19,15 @@ public class RestoreFolderFromTrashRequestBodyParentField extends SerializableOb
       RestoreFolderFromTrashRequestBodyParentFieldBuilder builder) {
     super();
     this.id = builder.id;
+    this.userId = builder.userId;
   }
 
   public String getId() {
     return id;
+  }
+
+  public String getUserId() {
+    return userId;
   }
 
   @Override
@@ -31,25 +40,40 @@ public class RestoreFolderFromTrashRequestBodyParentField extends SerializableOb
     }
     RestoreFolderFromTrashRequestBodyParentField casted =
         (RestoreFolderFromTrashRequestBodyParentField) o;
-    return Objects.equals(id, casted.id);
+    return Objects.equals(id, casted.id) && Objects.equals(userId, casted.userId);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id);
+    return Objects.hash(id, userId);
   }
 
   @Override
   public String toString() {
-    return "RestoreFolderFromTrashRequestBodyParentField{" + "id='" + id + '\'' + "}";
+    return "RestoreFolderFromTrashRequestBodyParentField{"
+        + "id='"
+        + id
+        + '\''
+        + ", "
+        + "userId='"
+        + userId
+        + '\''
+        + "}";
   }
 
   public static class RestoreFolderFromTrashRequestBodyParentFieldBuilder {
 
     protected String id;
 
+    protected String userId;
+
     public RestoreFolderFromTrashRequestBodyParentFieldBuilder id(String id) {
       this.id = id;
+      return this;
+    }
+
+    public RestoreFolderFromTrashRequestBodyParentFieldBuilder userId(String userId) {
+      this.userId = userId;
       return this;
     }
 
