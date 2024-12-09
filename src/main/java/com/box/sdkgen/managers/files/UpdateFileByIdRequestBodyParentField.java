50f7521427f6019e588286eb9e9be20a8e28c3c1
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-12-09 13:54:25 +0100

    feat: Support optional `userId` parameter for updating files, folders and web links (#106)

--- src/main/java/com/box/sdkgen/managers/files/UpdateFileByIdRequestBodyParentField.java
+++ src/main/java/com/box/sdkgen/managers/files/UpdateFileByIdRequestBodyParentField.java
@@ -1,11 +1,15 @@
 package com.box.sdkgen.managers.files;
 
 import com.box.sdkgen.internal.SerializableObject;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Objects;
 
 public class UpdateFileByIdRequestBodyParentField extends SerializableObject {
 
   protected String id;
+
+  @JsonProperty("user_id")
+  protected String userId;
 
   public UpdateFileByIdRequestBodyParentField() {
     super();
@@ -15,10 +19,15 @@ public class UpdateFileByIdRequestBodyParentField extends SerializableObject {
       UpdateFileByIdRequestBodyParentFieldBuilder builder) {
     super();
     this.id = builder.id;
+    this.userId = builder.userId;
   }
 
   public String getId() {
     return id;
+  }
+
+  public String getUserId() {
+    return userId;
   }
 
   @Override
@@ -30,25 +39,40 @@ public class UpdateFileByIdRequestBodyParentField extends SerializableObject {
       return false;
     }
     UpdateFileByIdRequestBodyParentField casted = (UpdateFileByIdRequestBodyParentField) o;
-    return Objects.equals(id, casted.id);
+    return Objects.equals(id, casted.id) && Objects.equals(userId, casted.userId);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id);
+    return Objects.hash(id, userId);
   }
 
   @Override
   public String toString() {
-    return "UpdateFileByIdRequestBodyParentField{" + "id='" + id + '\'' + "}";
+    return "UpdateFileByIdRequestBodyParentField{"
+        + "id='"
+        + id
+        + '\''
+        + ", "
+        + "userId='"
+        + userId
+        + '\''
+        + "}";
   }
 
   public static class UpdateFileByIdRequestBodyParentFieldBuilder {
 
     protected String id;
 
+    protected String userId;
+
     public UpdateFileByIdRequestBodyParentFieldBuilder id(String id) {
       this.id = id;
+      return this;
+    }
+
+    public UpdateFileByIdRequestBodyParentFieldBuilder userId(String userId) {
+      this.userId = userId;
       return this;
     }
 
