7b1f518ece99bec8f97a365755ed85df7036653b
box-sdk-build <sdk-build@box.com>
2025-02-24 05:09:32 -0800

    test: reorder function calls to fix test samples (box/box-codegen#666)

--- src/test/java/com/box/sdkgen/test/legalholdpolicies/LegalHoldPoliciesITest.java
+++ src/test/java/com/box/sdkgen/test/legalholdpolicies/LegalHoldPoliciesITest.java
@@ -15,6 +15,30 @@ public class LegalHoldPoliciesITest {
   private static final BoxClient client = getDefaultClient();
 
   @Test
+  public void testCreateNotOngoingLegalHoldPolicy() {
+    String legalHoldPolicyName = getUuid();
+    String legalHoldDescription = "test description";
+    String filterStartedAt = "2021-01-01T00:00:00-08:00";
+    String filterEndedAt = "2022-01-01T00:00:00-08:00";
+    LegalHoldPolicy legalHoldPolicy =
+        client
+            .getLegalHoldPolicies()
+            .createLegalHoldPolicy(
+                new CreateLegalHoldPolicyRequestBody.CreateLegalHoldPolicyRequestBodyBuilder(
+                        legalHoldPolicyName)
+                    .description(legalHoldDescription)
+                    .filterStartedAt(filterStartedAt)
+                    .filterEndedAt(filterEndedAt)
+                    .isOngoing(false)
+                    .build());
+    assert legalHoldPolicy.getPolicyName().equals(legalHoldPolicyName);
+    assert legalHoldPolicy.getDescription().equals(legalHoldDescription);
+    assert legalHoldPolicy.getFilterStartedAt().equals(filterStartedAt);
+    assert legalHoldPolicy.getFilterEndedAt().equals(filterEndedAt);
+    client.getLegalHoldPolicies().deleteLegalHoldPolicyById(legalHoldPolicy.getId());
+  }
+
+  @Test
   public void testCreateUpdateGetDeleteLegalHoldPolicy() {
     String legalHoldPolicyName = getUuid();
     String legalHoldDescription = "test description";
@@ -47,29 +71,5 @@ public class LegalHoldPoliciesITest {
                     .build());
     assert updatedLegalHoldPolicy.getPolicyName().equals(updatedLegalHoldPolicyName);
     client.getLegalHoldPolicies().deleteLegalHoldPolicyById(legalHoldPolicyId);
-  }
-
-  @Test
-  public void testCreateNotOngoingLegalHoldPolicy() {
-    String legalHoldPolicyName = getUuid();
-    String legalHoldDescription = "test description";
-    String filterStartedAt = "2021-01-01T00:00:00-08:00";
-    String filterEndedAt = "2022-01-01T00:00:00-08:00";
-    LegalHoldPolicy legalHoldPolicy =
-        client
-            .getLegalHoldPolicies()
-            .createLegalHoldPolicy(
-                new CreateLegalHoldPolicyRequestBody.CreateLegalHoldPolicyRequestBodyBuilder(
-                        legalHoldPolicyName)
-                    .description(legalHoldDescription)
-                    .filterStartedAt(filterStartedAt)
-                    .filterEndedAt(filterEndedAt)
-                    .isOngoing(false)
-                    .build());
-    assert legalHoldPolicy.getPolicyName().equals(legalHoldPolicyName);
-    assert legalHoldPolicy.getDescription().equals(legalHoldDescription);
-    assert legalHoldPolicy.getFilterStartedAt().equals(filterStartedAt);
-    assert legalHoldPolicy.getFilterEndedAt().equals(filterEndedAt);
-    client.getLegalHoldPolicies().deleteLegalHoldPolicyById(legalHoldPolicy.getId());
   }
 }
